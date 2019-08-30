541e408cdf269d543ce78e4b3caac6eb26c69f82
haoziapple <haozixiaowang@163.com>
2019-08-30 15:26:15 +0800

    添加mybatis-plus代码：分页查询，p6spy监控

--- plus-lab/src/main/java/github/haozi/mybatis/pluslab/entity/User.java
+++ plus-lab/src/main/java/github/haozi/mybatis/pluslab/entity/User.java
@@ -1,14 +1,20 @@
 package github.haozi.mybatis.pluslab.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Data
+@Accessors(chain = true)
 public class User {
     private Long id;
     private String name;
     private Integer age;
     private String email;
     private Date createDate;
+
+    @TableField(exist = false)
+    private Integer count;
 }
--- plus-lab/src/test/java/github/haozi/mybatis/pluslab/PlusLabApplicationTests.java
+++ plus-lab/src/test/java/github/haozi/mybatis/pluslab/PlusLabApplicationTests.java
@@ -1,5 +1,11 @@
 package github.haozi.mybatis.pluslab;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import github.haozi.mybatis.pluslab.entity.User;
 import github.haozi.mybatis.pluslab.mapper.UserMapper;
 import org.junit.Assert;
@@ -17,6 +23,8 @@ public class PlusLabApplicationTests {
     @Resource
     private UserMapper userMapper;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     public void contextLoads() {
     }
@@ -27,6 +35,39 @@ public class PlusLabApplicationTests {
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectCondition() {
+        System.out.println(("----- selectCondition method test: like function ------"));
+        List<User> userList1 = userMapper.selectList(new LambdaQueryWrapper<User>()
+                .like(User::getEmail, "test"));
+        userList1.forEach(System.out::println);
+
+        System.out.println(("----- selectCondition method test: or function ------"));
+        List<User> userList2 = userMapper.selectList(new LambdaQueryWrapper<User>()
+                .eq(User::getId, 1)
+                .or().eq(User::getId, 2));
+        userList2.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectPage() throws Throwable{
+        System.out.println(("----- selectPage method test ------"));
+        IPage<User> userIPage = userMapper.selectPage(new Page<>(1, 5),
+                new LambdaQueryWrapper<User>().in(User::getAge, 21, 24));
+        System.out.println(objectMapper.writeValueAsString(userIPage));
+    }
+
+    @Test
+    public void testSelectMaxId() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.select("max(id) as id");
+        User user = userMapper.selectOne(wrapper);
+        System.out.println(user);
+
+        List<User> users = userMapper.selectList(Wrappers.<User>lambdaQuery().orderByDesc(User::getId));
+        Assert.assertEquals(user.getId().longValue(), users.get(0).getId().longValue());
     }
 
 }
